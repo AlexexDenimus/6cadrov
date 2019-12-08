9cda2885b7f4a30b95edbc3e80711afad08143c8
AlexexDenimus <demin.leha1998@yandex.ru>
2019-12-08 13:56:01 +0300

    fix_bags

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState, useRef } from 'react';
-import { Box, Flex } from 'rebass';
-import styled from 'styled-components'
-import { Root } from './components/Root';
-import { ProgressBar } from './components/ProgressBar';
+import React, { useState, useRef } from "react";
+import { Box, Flex } from "rebass";
+import styled from "styled-components";
+import { Root } from "./components/Root";
+import { ProgressBar } from "./components/ProgressBar";
 
 const Heading = styled.h1`
   font-size: 72px;
@@ -11,10 +11,10 @@ const Heading = styled.h1`
 const Body = styled.div`
   width: 900px;
   margin: 300px auto 0;
-`
+`;
 
 function App() {
-  const [AType, setAType] = useState('');
+  const [AType, setAType] = useState("");
   const [A, setA] = useState(0);
   const [Ch, setCh] = useState(0);
   const [DP, setDP] = useState(0);
@@ -27,7 +27,7 @@ function App() {
   const [distance, setDistance] = useState(5);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const currentProgress = currentPage/distance*100;
+  const currentProgress = (currentPage / distance) * 100;
 
   const handleFirstClick = () => {
     if (AType === "current") {
@@ -37,142 +37,294 @@ function App() {
       setCurrentPage(2);
       setDistance(3);
     }
-    setAType('');
-  }
+    setAType("");
+  };
 
   const handleNextClick = () => {
     setCurrentPage(currentPage + 1);
-  }
+  };
 
   const handleLastClick = () => {
     setCurrentPage(currentPage + 1);
     setA(DP + Ch);
-  }
+  };
 
-  const handleAChange = (event) => {
+  const handleAChange = event => {
     event.preventDefault();
-    setA(parseInt(Chr.current.value) * parseInt(Kn.current.value))
-  }
+    setA(parseInt(Chr.current.value) * parseInt(Kn.current.value));
+  };
 
-  const handleDPChange = (event) => {
+  const handleDPChange = event => {
     event.preventDefault();
-    if (AType === 'upgrade'){
+    if (AType === "upgrade") {
       if (parseInt(Apl.current.value) < parseInt(Ab.current.value)) {
         return setDP(NaN);
       }
-      setDP(parseInt(Apl.current.value) - parseInt(Ab.current.value))
+      setDP(parseInt(Apl.current.value) - parseInt(Ab.current.value));
       return;
     }
-    if (AType === 'swap'){
-      setDP(parseInt(Apl.current.value) * parseInt(Ab.current.value))
+    if (AType === "swap") {
+      setDP(parseInt(Apl.current.value) * parseInt(Ab.current.value));
       return;
     }
-  }
+  };
 
-  const handleChChange = (event) => {
+  const handleChChange = event => {
     event.preventDefault();
-    if ((parseInt(B.current.value) === 0) && (parseInt(OP.current.value) > 0)) {
+    if (parseInt(B.current.value) === 0 && parseInt(OP.current.value) > 0) {
       return setCh(NaN);
     }
-    return setCh(parseInt(OP.current.value) / parseInt(B.current.value))
-  }
-  
+    return setCh(parseInt(OP.current.value) / parseInt(B.current.value));
+  };
+
   return (
     <Body>
-    <ProgressBar progress={currentProgress} />
-      {(currentPage === 1) && 
-        (<Root title="Выберите период для рассчёта потребности персонала (A)" onClick={handleFirstClick} disabled={AType === '' ? true : false}>
+      <ProgressBar progress={currentProgress} />
+      {currentPage === 1 && (
+        <Root
+          title="Выберите период для рассчёта потребности персонала (A)"
+          onClick={handleFirstClick}
+          disabled={AType === "" ? true : false}
+        >
           <Box>
-            <input type="radio" id="choice1"
-              name="A" value="current" onChange={e => setAType(e.target.value)} />
+            <input
+              type="radio"
+              id="choice1"
+              name="A"
+              value="current"
+              onChange={e => setAType(e.target.value)}
+            />
             <label htmlFor="choice1">Текущая потребность в специалиста</label>
           </Box>
           <Box>
-            <input type="radio" id="choice2"
-              name="A" value="long" onChange={e => setAType(e.target.value)}  />
-            <label htmlFor="choice2">Долговременная потребность в специалиста</label>
+            <input
+              type="radio"
+              id="choice2"
+              name="A"
+              value="long"
+              onChange={e => setAType(e.target.value)}
+            />
+            <label htmlFor="choice2">
+              Долговременная потребность в специалиста
+            </label>
           </Box>
-        </Root>) 
-      }
-        {(currentPage === 2 && distance === 3) && 
-          (<Root title="Введите данные для вычисления потребности персонала" onClick={handleNextClick} disabled={isNaN(A) ? true : false}>
-            <Flex flexDirection="row" alignItems="center" justifyContent="space-between">
-            <label htmlFor="workerCount">Среднесписочна численность рабочих</label>
-              <input type="number" id="workerCount"
-                name="Chr" min='0' defaultValue="0" onChange={handleAChange} ref={Chr}/>
-            </Flex>
-            <Flex flexDirection="row" alignItems="center" justifyContent="space-between">
-            <label htmlFor="coefficient">Нормативный коэффициент насыщенности специалистами</label>
-              <input type="number" id="coefficient"
-                name="Kn" min='0' defaultValue="0" onChange={handleAChange} ref={Kn} />
-            </Flex>
-          </Root>) 
-        }
-        {(currentPage === 2 && distance === 5) && 
-          (<Root title="Введите данные для вычисления базовой потребности в кадрах" onClick={handleNextClick} disabled={isNaN(Ch) ? true : false}>
-            <h3>Ч = ОП / В = {Ch.toFixed(2)}</h3>
-            <Flex flexDirection="row" alignItems="center" justifyContent="space-between">
+        </Root>
+      )}
+      {currentPage === 2 && distance === 3 && (
+        <Root
+          title="Введите данные для вычисления потребности персонала"
+          onClick={handleNextClick}
+          disabled={isNaN(A) ? true : false}
+        >
+          <Flex
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <label htmlFor="workerCount">
+              Среднесписочна численность рабочих
+            </label>
+            <input
+              type="number"
+              id="workerCount"
+              name="Chr"
+              min="0"
+              defaultValue="0"
+              onChange={handleAChange}
+              ref={Chr}
+            />
+          </Flex>
+          <Flex
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <label htmlFor="coefficient">
+              Нормативный коэффициент насыщенности специалистами
+            </label>
+            <input
+              type="number"
+              id="coefficient"
+              name="Kn"
+              min="0"
+              defaultValue="0"
+              onChange={handleAChange}
+              ref={Kn}
+            />
+          </Flex>
+        </Root>
+      )}
+      {currentPage === 2 && distance === 5 && (
+        <Root
+          title="Введите данные для вычисления базовой потребности в кадрах"
+          onClick={handleNextClick}
+          disabled={isNaN(Ch) ? true : false}
+        >
+          <h3>Ч = ОП / В = {Math.ceil(Ch)} человек</h3>
+          <Flex
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
             <label htmlFor="volume">Объем производства (ОП)</label>
-              <input type="number" id="volume"
-                name="OP" min='0' defaultValue="0" onChange={handleChChange} ref={OP}/>
-            </Flex>
-            <Flex flexDirection="row" alignItems="center" justifyContent="space-between">
-            <label htmlFor="singleWorker">Выработка на одного работающего (В)</label>
-              <input type="number" id="singleWorker"
-                name="B" min='0' defaultValue="0" onChange={handleChChange} ref={B} />
-            </Flex>
-          </Root>) 
-        }
-        {(currentPage === 3 && distance === 5) && 
-          (<Root title="Для каких целей возникла дополнительная потребность в кадрах" onClick={handleNextClick} disabled={AType === '' ? true : false}>
-            <Box>
-              <input type="radio" id="choice3"
-                name="DP" value="upgrade" onChange={e => setAType(e.target.value)} />
-              <label htmlFor="choice3">Происходит развитие предприятия</label>
-            </Box>
-            <Box>
-              <input type="radio" id="choice4"
-                name="DP" value="swap" onChange={e => setAType(e.target.value)}  />
-              <label htmlFor="choice4">Частичная замена специалистов</label>
-            </Box>
-          </Root>) 
-        }
-        {(currentPage === 4 && distance === 5 && AType === 'upgrade') && 
-          (<Root title="Введите данные для вычисления дополнительной потребности в кадрах" onClick={handleLastClick} disabled={isNaN(DP) ? true : false}>
-            <h3>ДП = Апл - Аб = {DP}</h3>
-            <Flex flexDirection="row" alignItems="center" justifyContent="space-between">
-            <label htmlFor="common">Общая потребность в специалистах в планируемый период (Апл)</label>
-              <input type="number" id="common"
-                name="Apl" min='0' defaultValue="0" onChange={handleDPChange} ref={Apl}/>
-            </Flex>
-            <Flex flexDirection="row" alignItems="center" justifyContent="space-between">
-            <label htmlFor="basic">Общая потребность в специалистах в базовый период (Аб)</label>
-              <input type="number" id="basic"
-                name="Ab" min='0' defaultValue="0" onChange={handleDPChange} ref={Ab} />
-            </Flex>
-          </Root>) 
-        }
-          {(currentPage === 4 && distance === 5 && AType === 'swap') && 
-            (<Root title="Введите данные для вычисления дополнительной потребности в кадрах" onClick={handleLastClick} disabled={isNaN(DP) ? true : false}>
-              <h3>ДП = Апл * Кв = {DP.toFixed()}</h3>
-              <Flex flexDirection="row" alignItems="center" justifyContent="space-between">
-              <label htmlFor="common">Общая потребность в специалистах в планируемый период (Апл)</label>
-                <input type="number" id="common"
-                  name="Apl" min='0' defaultValue="0" onChange={handleDPChange} ref={Apl}/>
-              </Flex>
-              <Flex flexDirection="row" alignItems="center" justifyContent="space-between">
-              <label htmlFor="basic">Коэффициент выбытия специалистов (Кв)</label>
-                <input type="number" id="basic"
-                  name="Ab" min='0' defaultValue="0" onChange={handleDPChange} ref={Ab} />
-              </Flex>
-            </Root>) 
-          }
-          {((currentPage === 5 && distance === 5) || (currentPage === 3 && distance === 3)) && 
-            (<Root title="Общая потребность предприятия в кадрах составила:" noButton>
-                <Box alignSelf="center"><Heading>{A.toFixed()}</Heading></Box>
-            </Root>) 
-          }
-        </Body>
+            <input
+              type="number"
+              id="volume"
+              name="OP"
+              min="0"
+              defaultValue="0"
+              onChange={handleChChange}
+              ref={OP}
+            />
+          </Flex>
+          <Flex
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <label htmlFor="singleWorker">
+              Выработка на одного работающего (В)
+            </label>
+            <input
+              type="number"
+              id="singleWorker"
+              name="B"
+              min="0"
+              defaultValue="0"
+              onChange={handleChChange}
+              ref={B}
+            />
+          </Flex>
+        </Root>
+      )}
+      {currentPage === 3 && distance === 5 && (
+        <Root
+          title="Для каких целей возникла дополнительная потребность в кадрах"
+          onClick={handleNextClick}
+          disabled={AType === "" ? true : false}
+        >
+          <Box>
+            <input
+              type="radio"
+              id="choice3"
+              name="DP"
+              value="upgrade"
+              onChange={e => setAType(e.target.value)}
+            />
+            <label htmlFor="choice3">Происходит развитие предприятия</label>
+          </Box>
+          <Box>
+            <input
+              type="radio"
+              id="choice4"
+              name="DP"
+              value="swap"
+              onChange={e => setAType(e.target.value)}
+            />
+            <label htmlFor="choice4">Частичная замена специалистов</label>
+          </Box>
+        </Root>
+      )}
+      {currentPage === 4 && distance === 5 && AType === "upgrade" && (
+        <Root
+          title="Введите данные для вычисления дополнительной потребности в кадрах"
+          onClick={handleLastClick}
+          disabled={isNaN(DP) ? true : false}
+        >
+          <h3>ДП = Апл - Аб = {DP} человек</h3>
+          <Flex
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <label htmlFor="common">
+              Общая потребность в специалистах в планируемый период (Апл)
+            </label>
+            <input
+              type="number"
+              id="common"
+              name="Apl"
+              min="0"
+              defaultValue="0"
+              onChange={handleDPChange}
+              ref={Apl}
+            />
+          </Flex>
+          <Flex
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <label htmlFor="basic">
+              Общая потребность в специалистах в базовый период (Аб)
+            </label>
+            <input
+              type="number"
+              id="basic"
+              name="Ab"
+              min="0"
+              defaultValue="0"
+              onChange={handleDPChange}
+              ref={Ab}
+            />
+          </Flex>
+        </Root>
+      )}
+      {currentPage === 4 && distance === 5 && AType === "swap" && (
+        <Root
+          title="Введите данные для вычисления дополнительной потребности в кадрах"
+          onClick={handleLastClick}
+          disabled={isNaN(DP) ? true : false}
+        >
+          <h3>ДП = Апл * Кв = {DP.toFixed()} человек</h3>
+          <Flex
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <label htmlFor="common">
+              Общая потребность в специалистах в планируемый период (Апл)
+            </label>
+            <input
+              type="number"
+              id="common"
+              name="Apl"
+              min="0"
+              defaultValue="0"
+              onChange={handleDPChange}
+              ref={Apl}
+            />
+          </Flex>
+          <Flex
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <label htmlFor="basic">Коэффициент выбытия специалистов (Кв)</label>
+            <input
+              type="number"
+              id="basic"
+              name="Ab"
+              min="0"
+              defaultValue="0"
+              onChange={handleDPChange}
+              ref={Ab}
+            />
+          </Flex>
+        </Root>
+      )}
+      {((currentPage === 5 && distance === 5) ||
+        (currentPage === 3 && distance === 3)) && (
+        <Root
+          title="Общая потребность предприятия в кадрах составила:"
+          noButton
+        >
+          <Box alignSelf="center">
+            <Heading>{A.toFixed()} человек</Heading>
+          </Box>
+        </Root>
+      )}
+    </Body>
   );
 }
 
